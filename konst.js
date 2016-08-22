35129ee29b25541842ad8c89fc3ce10a8ef59209
fredrikbryntesson <fredrik.bryntesson@vidhance.com>
2016-08-22 23:24:34 +0200

    Add

---
+++ konst.js
@@ -0,0 +1,3 @@
+
+
+Konst i alla dess former mår bra av ramar, ramar som markerar tillhörighet, som markerar avståndstagande mot "de andra" ramar som sprängs och blir till nya ramar för att spränga, i det kretsloppet som kallas konsthistoria. Eller, i det här fallet, mjukvaruhistoria.
